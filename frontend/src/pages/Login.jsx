44b6dd7f108238193862deb1d22a53f4c4e97edb
Juan Pablo Romero <romerojuanpablo.00@gmail.com>
2025-10-06 15:38:56 -0300

    feat: sincronización con cambios remotos + configuración local

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { useNavigate } from 'react-router-dom';
+import imgHero from '../../public/img/carwash-section.png';
 
 const Login = () => {
   const navigate = useNavigate();
@@ -9,11 +10,12 @@ const Login = () => {
     <div>
       <Header />
       <main className="login-main">
-        <div className="login-left">
-          <div className="login-logo">CarwashFreaks</div>
-        </div>
-
-        <div className="login-right">
+        <section className="login-section">
+          <div className="login-left">
+            {/* <img src={imgHero} alt="CarwashFreaks" className="login-img" /> */}
+            <div className="login-logo">CarwashFreaks</div>
+          </div>
+          <div className="login-right">
             <form className="login-form">
               <h2>Iniciar sesión</h2>
               <input type="email" placeholder="Correo electrónico" required />
@@ -21,7 +23,8 @@ const Login = () => {
               <button type="submit" className="login-btn-full">Iniciar sesión</button>
               <button type="button" className="register-btn" onClick={() => navigate('/register')}>Registrarse</button>
             </form>
-        </div>
+          </div>
+        </section>
       </main>
       <Footer />
     </div>
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import { useNavigate } from 'react-router-dom';
+import imgHero from '../../public/img/carwash-section.png';
 
 const Register = () => {
   const navigate = useNavigate();
@@ -9,11 +10,12 @@ const Register = () => {
     <div>
       <Header />
       <main className="login-main">
-        <div className="login-left">
-          <div className="login-logo">CarwashFreaks</div>
-        </div>
-
-        <div className="login-right">
+        <section className="login-section">
+          <div className="login-left">
+            {/* <img src={imgHero} alt="CarwashFreaks" className="login-img" /> */}
+            <div className="login-logo">CarwashFreaks</div>
+          </div>
+          <div className="login-right">
             <form className="login-form">
               <h2>Registrarse</h2>
               <input type="text" placeholder="Nombre" required />
@@ -25,6 +27,7 @@ const Register = () => {
               <button type="button" className="register-btn" onClick={() => navigate('/login')}>Iniciar sesión</button>
             </form>
           </div>
+        </section>
       </main>
       <Footer />
     </div>
